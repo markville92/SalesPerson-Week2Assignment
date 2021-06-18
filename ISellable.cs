6ab3cc0f57dfc3f4704a5820ebc9ac8ccbd93c72
markville92 <77904385+markville92@users.noreply.github.com>
2021-06-18 15:10:56 -0500

    Add files via upload

---
+++ ISellable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesPerson_Wk2
+{
+
+    interface ISellable
+    {
+        void SalesSpeech();
+        void MakeSale(int val);
+    }
+}
